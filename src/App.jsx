021eb648a13c264bc0b4478421c29cb36312588b
abhishek.jangid <abhishek.jangid@owebest.in>
2025-07-19 08:50:16 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,44 @@ import Category from "./pages/Category";
 export default function App() {
   return (
     // <Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/forgot-password" element={<ForgotPassword />} />
+
+    //     <Route
+    //       path="/"
+    //       element={
+    //           <Layout />
+    //         // <ProtectedRoute>
+    //         // </ProtectedRoute>
+    //       }
+    //     >
+    //       <Route path="/" element={<Dashboard />} />
+    //       <Route path="category" element={<Category />} />
+
+    //       <Route path="products/list" element={<ProductList />} />
+    //       <Route path="products/create" element={<CreateProduct />} />
+
+    //       <Route
+    //         path="/settings/privacypolicy"
+    //         element={<CMSPrivacyEditor />}
+    //       />
+    //       <Route path="*" element={<Dashboard />} />
+    //     </Route>
+    //   </Routes>
+    // </Router>
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        <Route
-          path="/"
-          element={
-              <Layout />
-            // <ProtectedRoute>
-            // </ProtectedRoute>
-          }
-        >
-          <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
           <Route path="category" element={<Category />} />
-
           <Route path="products/list" element={<ProductList />} />
           <Route path="products/create" element={<CreateProduct />} />
-
-          <Route
-            path="/settings/privacypolicy"
-            element={<CMSPrivacyEditor />}
-          />
+          <Route path="settings/privacypolicy" element={<CMSPrivacyEditor />} />
           <Route path="*" element={<Dashboard />} />
         </Route>
       </Routes>
